9b2f2e795eeb5383aa9ebe59da6f916a73679c7d
raju2806 <raju7654030601@gmail.com>
2024-06-27 01:44:14 +0530

    update

--- prime_between1to100.cpp
+++ prime_between1to100.cpp
@@ -8,15 +8,15 @@ int main()
     cin>>a>>b;
     int flag=0;
     if(a>b) {swap(a,b);}   
-    for(i=a; i<b/2; i++)
+    for(i=a; i<b; i++)
     {
         if(i==1 || i==0) {continue;}  
         flag=0;
         for(j=2; j<=i/2; j++)
         {
             if(i%j==0) 
-            flag=1;
-            break;
+            {flag=1;
+            break;}
         }
         if(flag==0){cout<<i<<" ";}
     }
