f9a27da37283da422e1a7ade644e3290ad050005
raju2806 <raju7654030601@gmail.com>
2024-06-28 20:40:08 +0530

    update

--- stirng.cpp
+++ stirng.cpp
@@ -7,9 +7,11 @@ int main()
 {
     string s;
     cin>>s;
-    int num = 1,i=0; 
-    while(s[i]!='\0') {i++;}
-    for(int t = 1; t < i; t++) {num = num * 10 + (s[t] - '0');} 
+    int num = 0,i=0; 
+    // while(s[i]!='\0') {i++;}
+    // for(int t = 1; t < i; t++) {num = num * 10 + (s[t] - '0');} 
+    for(auto x: s){num = num * 10 + (x - '0');}
+    cout<<num<<endl;
     if(num%3==0){cout<<"Yes"<<endl; }
     else {cout<<"No"<<endl;}
     return 0;
