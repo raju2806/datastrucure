a2d79e45175a190447dc2d1f3e2039e2ed6ea995
raju2806 <raju7654030601@gmail.com>
2024-06-28 12:47:41 +0530

    divisiblestring

--- stirng.cpp
+++ stirng.cpp
@@ -7,9 +7,10 @@ int main()
 {
     string s;
     cin>>s;
-    long long n = stoi(s);
-
-    if(n%3==0){cout<<"Yes"<<endl; }
-    else {cout<<"No";}
+    int num = 1,i=0; 
+    while(s[i]!='\0') {i++;}
+    for(int t = 1; t < i; t++) {num = num * 10 + (s[t] - '0');} 
+    if(num%3==0){cout<<"Yes"<<endl; }
+    else {cout<<"No"<<endl;}
     return 0;
 }
