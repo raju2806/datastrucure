17b1b1f7d8a683142aa34cf0bbc3f75149967625
raju2806 <raju7654030601@gmail.com>
2024-06-29 12:55:17 +0530

    add-k

--- sliding_window.cpp
+++ sliding_window.cpp
@@ -4,33 +4,35 @@ using namespace std;
 
 void sliding_window(int arr[], int n, int k)
 {
-    int max=INT_MIN;
-    for (int i = 0; i <= n-k; i++)
-    {
-        for(int j=1; j<k; j++)
-        {
-            if(arr[i+j]>max){max=arr[i+j];}
-        }
-        cout<<max<<" ";
+    int sumk;
+    for (int i = 0; i < n-k+1; i++) {
+        sumk = 0;
+        for (int j = i; j < k+i; j++) {sumk += arr[j];}
+    cout<<sumk<<" ";
     }
     
+ 
 }
 
 int main()
 {
-    int n,k;
+    int n,k,sum;
+    
+    cout<<"Enter size of array: ";
+    cin>>n;
     int arr[n];
     cout<<"Enter window size: ";
     cin>>k;
-
-    cout<<"Enter size of array: ";
-    cin>>n;
     cout<<"Enter array element: ";
     for(int i=0; i<n; i++){cin>>arr[i];}
 
+    // for (int i = 0; i < n-k+1; i++) {
+    //     sum = 0;
+    //     for (int j = i; j < k+i; j++) {sum += arr[j];}
+    // cout<<sum<<" ";
+    // }
+
     sliding_window(arr,n,k);
-
-
     return 0;
 
 }
